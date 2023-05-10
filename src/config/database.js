970fc78d206761847382153b3517afcf0de84d50
floxydio <diooktar@gmail.com>
2023-05-11 01:39:58 +0700

    update

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,13 @@
 const mysql = require("mysql2");
+const dotenv = require("dotenv");
 
+dotenv.config();
 // create the connection to database
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "tugasakhir",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
 
 connection.connect(function (err) {
