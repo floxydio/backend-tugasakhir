74d4b5071792710aca37f9d5653f67a8ac7b2635
floxydio <diooktar@gmail.com>
2023-05-14 14:47:36 +0700

    add string

--- src/repository/absen.repo.js
+++ src/repository/absen.repo.js
@@ -2,7 +2,7 @@ const { connection } = require("../config/database.js");
 
 function sendAbsence(body, callback) {
   connection.query(
-    `INSERT INTO absen(user_id, guru_id, pelajaran_id, kelas_id, keterangan,reason, createdAt) VALUES ('${body.user_id}','${body.guru_id}', '${body.pelajaran_id}','${body.kelas_id}', '${body.keterangan}',${body.reason} ,'${body.createdAt}')`,
+    `INSERT INTO absen(user_id, guru_id, pelajaran_id, kelas_id, keterangan,reason, createdAt) VALUES ('${body.user_id}','${body.guru_id}', '${body.pelajaran_id}','${body.kelas_id}', '${body.keterangan}','${body.reason}' ,'${body.createdAt}')`,
     function (err, result) {
       if (err) {
         console.log(err);
