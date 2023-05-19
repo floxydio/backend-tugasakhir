a44a563a564d00f052feb1a6f14371878d8c5c32
floxydio <diooktar@gmail.com>
2023-05-20 00:00:39 +0700

    update absen repo

--- src/repository/absen.repo.js
+++ src/repository/absen.repo.js
@@ -29,13 +29,16 @@ function getAbsenByUserId(id, callback) {
 }
 
 function getAbsen(callback) {
-  connection.query(`SELECT * FROM absen`, function (err, result) {
-    if (err) {
-      callback(err, null);
-    } else {
-      callback(null, result);
+  connection.query(
+    `SELECT absen.id, users.nama as nama_user,guru.nama as nama_guru,pelajaran.nama as pelajaran_nama,kelas.nomor as nomor_kelas,absen.keterangan,absen.day,absen.month,absen.year,absen.time FROM absen LEFT JOIN users ON absen.user_id = users.id LEFT JOIN pelajaran ON absen.pelajaran_id = pelajaran.id LEFT JOIN kelas ON absen.kelas_id = kelas.id LEFT JOIN guru ON absen.guru_id = guru.id`,
+    function (err, result) {
+      if (err) {
+        callback(err, null);
+      } else {
+        callback(null, result);
+      }
     }
-  });
+  );
 }
 
 module.exports = { sendAbsence, getAbsenByUserId, getAbsen };
