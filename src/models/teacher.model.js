d87c7a0fc26cf9986ca9eb8ddd40088601e06dc8
floxydio <diooktar@gmail.com>
2023-05-20 15:45:35 +0700

    guru fix

--- src/models/teacher.model.js
+++ src/models/teacher.model.js
@@ -1,7 +1,6 @@
 class TeacherEntity {
-  constructor(nama, umur, mengajar, status_guru, rating) {
+  constructor(nama, mengajar, status_guru, rating) {
     this.nama = nama;
-    this.umur = umur;
     this.mengajar = mengajar;
     this.status_guru = status_guru;
     this.rating = rating;
