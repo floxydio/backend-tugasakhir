7ebf839a1c9447bfbc998ba6de4a03dbc4364217
floxydio <diooktar@gmail.com>
2023-06-03 00:36:53 +0700

    update pool

--- src/config/database.js
+++ src/config/database.js
@@ -14,12 +14,4 @@ const connection = mysql.createPool({
   waitForConnections: true,
 });
 
-connection.connect(function (err) {
-  if (err) {
-    console.log(`Error connecting to database ${err}`);
-  } else {
-    console.log("🔥 Database Connected");
-  }
-});
-
 module.exports = { connection };
