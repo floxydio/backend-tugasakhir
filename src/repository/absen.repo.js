eb0d4bd34fc801f9921d035d9cb253a0fbcc6c73
floxydio <diooktar@gmail.com>
2023-06-11 12:30:04 +0700

    absen detail by id and month

--- src/repository/absen.repo.js
+++ src/repository/absen.repo.js
@@ -40,7 +40,7 @@ function getAbsenByUserId(id, month, callback) {
 }
 
 function getAbsenByDetailandUserId(userId,month,callback) {
-  connection.query(`SELECT absen.id, users.nama as nama_user,guru.nama as nama_guru,pelajaran.nama as pelajaran_nama,kelas.nomor as nomor_kelas,absen.keterangan,absen.day,absen.month,absen.year,absen.time,absen.reason FROM absen LEFT JOIN users ON absen.user_id = users.id LEFT JOIN pelajaran ON absen.pelajaran_id = pelajaran.id LEFT JOIN kelas ON absen.kelas_id = kelas.nomor LEFT JOIN guru ON absen.guru_id = guru.id WHERE absen.user_id = ${userId} AND abssen.month = ${month}`, function(err,result) {
+  connection.query(`SELECT absen.id, users.nama as nama_user,guru.nama as nama_guru,pelajaran.nama as pelajaran_nama,kelas.nomor as nomor_kelas,absen.keterangan,absen.day,absen.month,absen.year,absen.time,absen.reason FROM absen LEFT JOIN users ON absen.user_id = users.id LEFT JOIN pelajaran ON absen.pelajaran_id = pelajaran.id LEFT JOIN kelas ON absen.kelas_id = kelas.nomor LEFT JOIN guru ON absen.guru_id = guru.id WHERE absen.user_id = ${userId} AND absen.month = ${month}`, function(err,result) {
     if (err) {
       callback(err, null);
     } else {
